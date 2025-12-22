8509e78ca1f0831e12567427e410848f0efffbf3
鍾 <148304414+ai1108@users.noreply.github.com>
2025-12-22 11:10:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -891,9 +891,3 @@ app.post("/webhook", (req, res) => {
     fulfillmentText: reply
   });
 });
-
-// 最後啟動 Express server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
